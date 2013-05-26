b7a6478cc8b3ac8ba5d183c0d358a584075f71f0
Justin Hewlett <hewlett.jus@gmail.com>
2013-05-25 23:38:50 -0600

    adding one more file to commit

--- test/lexer.Test.js
+++ test/lexer.Test.js
@@ -1,11 +1,35 @@
-module('getNextToken');
+module('All Tests');
 
-test("Single token with single rule is returned", function() {
+test("Single token with simple rule returns the value", function() {
     var lexer = new JSLex.Lexer();
     lexer.init('0',
     [
         [/[0-9]/, 'DIGIT']
     ]);
 
-    equal('DIGIT', lexer.getNextToken());
+    assertEquals('DIGIT', lexer.getNextToken());
+});
+
+test("Single token with function returns the value of the function", function() {
+    var lexer = new JSLex.Lexer();
+    lexer.init('a',
+        [
+            [/a/, function(text) {return text + text;}]
+        ]);
+
+    assertEquals('aa', lexer.getNextToken());
+});
+
+test("First matching rule has a side effect, second matching rule returns its value, third matching rule is ignored", function() {
+    var lexer = new JSLex.Lexer();
+    var capture = '';
+    lexer.init('a',
+        [
+            [/./, function() { capture = 'sideEffect1'; }],
+            [/a/, 'LETTER'],
+            [/./, function() { capture = 'sideEffect2'; }]
+        ]);
+
+    assertEquals('LETTER', lexer.getNextToken());
+    assertEquals('sideEffect1', capture);
 });
